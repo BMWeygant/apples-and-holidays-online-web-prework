73ee4687bb8d6ff16f3843bdb06850d08ff59efb
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-15 11:34:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.values do |keys, values|
-  if values == "BBQ"
-end
+ values == "BBQ"
+
 end
