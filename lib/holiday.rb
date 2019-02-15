6bad6b38e0096b294d63308b28f391c9ee668fdc
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-15 12:03:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,4 +76,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
    holidays.map do |holiday, supplies|
      holiday if supplies.include?("BBQ")
+   end
+ end.flatten
 end
